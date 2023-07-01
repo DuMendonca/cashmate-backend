90db038685a566b9be2f6b70099d11c8ec1ef2e0
eduardo.mendonca <eduardo.mendonca@neomind.com.br>
2023-07-01 01:56:19 -0300

    Initial Commit

---
+++ src/main/java/org/udesc/program/cashmate/CaseMateApplication.java
@@ -0,0 +1,13 @@
+package org.udesc.program.cashmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/udesc/program/cashmate/CaseMateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.udesc.program.cashmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
